79fd6068c99bd2ed04fb63e093f1dbaea2ab65d2
Julian Rojas <129390687+judali05@users.noreply.github.com>
2024-02-06 07:49:04 -0500

    Update ejercicio2.js

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -10,3 +10,5 @@ console.log("la resta de 4-1 es= "+mat.restar(4,1));
 console.log("la division de 6/2 es= "+mat.dividir(6,2));
 
 console.log("el valor de PI es= "+mat.PI);
+
+console.log("el valor de 4 exponenciado a la 3 es = "+mat.exponenciar(4,3));
