67bd70085ec01c43a87a63e0df6dce2fdd83fcdd
Julian Rojas <129390687+judali05@users.noreply.github.com>
2024-02-08 12:03:37 -0500

    Delete js directory

--- js/ejercicio3.js
+++
@@ -1,9 +0,0 @@
-const os=require('os');
-
-console.log("sistema operativo: "+os.platform());
-
-console.log("version del sistema operativo: "+os.release());
-
-console.log("memoria total: "+os.totalmem()+" bytes");
-
-console.log("memoria libre: "+os.freemem()+" bytes");
