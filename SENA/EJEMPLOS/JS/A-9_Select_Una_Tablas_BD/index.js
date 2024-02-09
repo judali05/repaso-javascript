f3b25357cda399ae9abe6fa88a8210f93f819608
Julian Rojas <129390687+judali05@users.noreply.github.com>
2024-02-09 15:43:44 -0500

    Add files via upload

---
+++ SENA/EJEMPLOS/JS/A-9_Select_Una_Tablas_BD/index.js
@@ -0,0 +1,20 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "mydb"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con.query("SELECT name, address FROM customers", function(err, result, fields) {
+        if (err) throw err;
+        console.log("selected:");
+        console.log(result);
+        con.end(); 
+    });
+});
+
