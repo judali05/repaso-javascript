18dccfbd58dfa7aa4d557eb7371a810264e1f65e
Julian Rojas <129390687+judali05@users.noreply.github.com>
2024-02-09 15:37:11 -0500

    Add files via upload

---
+++ SENA/EJEMPLOS/JS/A-8_Insertat_Datos_En_BD/index.js
@@ -0,0 +1,34 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "mydb"
+});
+
+//insertar un resgistor
+/*
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "INSERT INTO customers (name, address) VALUES ('julian', 'Cra 6')";
+        if (err) throw err;
+        console.log("Inserted");
+        con.end(); 
+    });
+});
+*/
+
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "INSERT INTO customers (name, address) VALUES ('julian', 'Cra 6'), ('juan', 'Cra 23'), ('luis', 'Cll 23'), ('carlos', 'Cll 3'), ('martha', 'trasversal 11'), ('maria', 'Cll 43')"; 
+    con.query(sql, function(err, result) {
+        if (err) throw err;
+        console.log("Inserted");
+        con.end(); 
+    });
+});
+
