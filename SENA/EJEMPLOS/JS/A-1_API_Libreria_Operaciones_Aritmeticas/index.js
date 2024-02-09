39c715f78f4ccd5f75f8834a25ffa0f97dd4f538
Julian Rojas <129390687+judali05@users.noreply.github.com>
2024-02-09 15:13:59 -0500

    Update index.js

--- SENA/EJEMPLOS/JS/A-1_API_Libreria_Operaciones_Aritmeticas/index.js
+++ SENA/EJEMPLOS/JS/A-1_API_Libreria_Operaciones_Aritmeticas/index.js
@@ -4,11 +4,7 @@ const mat=require('./matematica')
 
 //encapsulamiento
 console.log("la suma de 2+2 es= "+mat.sumar(2,2));
-
 console.log("la resta de 4-1 es= "+mat.restar(4,1));
-
 console.log("la division de 6/2 es= "+mat.dividir(6,2));
-
 console.log("el valor de PI es= "+mat.PI);
-
 console.log("el valor de la exponenciacion = " + mat.exponenciar(5,4));
