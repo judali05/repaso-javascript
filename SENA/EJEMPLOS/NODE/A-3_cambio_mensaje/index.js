3f7fae054e4188b8fabc084b97d64bf3e453a0b1
Julian Rojas <129390687+judali05@users.noreply.github.com>
2024-02-13 07:22:56 -0500

    Update index.js

--- SENA/EJEMPLOS/NODE/A-3_cambio_mensaje/index.js
+++ SENA/EJEMPLOS/NODE/A-3_cambio_mensaje/index.js
@@ -8,3 +8,5 @@ app.get("/Mensaje", function(req, res){
 app.listen(3000,function(){
     console.log("aplicacion ejemplo, escuchando el puerto 3000");
 });
+
+//se puede conectar a otro computadro con la ip de el computador por ejemplo http://10.175.81.244:3000
